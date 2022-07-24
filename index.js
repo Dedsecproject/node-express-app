4bf113bfbda159c2f5dcbfb9de0a837682689ff9
Luis Collymoore <dedseq@macbook-pro-2.myfiosgateway.com>
2022-07-24 13:05:09 -0400

    made changes to how the app behaves

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+app.use(express.json())
+
 
 let notes = [
   {
@@ -27,14 +29,56 @@ let notes = [
  //   response.writeHead(200, { 'Content-Type': 'application/json' })
 //    response.end(JSON.stringify(notes)) 
 //})
+const generatedId = () =>{
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(note => n.id))
+  :0
+ return maxId + 1
+}
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if(!body.content){
+    return response.status(400).json({
+      error: 'content missing'
+  })
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+  notes = notes.concat(note)
+  response.json(note)
+})
+
 
 
 app.get('/', (request, response)=>{
     response.send('<h1>Hello World!</h1>')
 })
-
+//get all notes
 app.get('/api/notes', (request, response)=>{
     response.json(notes)
+})
+//get specific notes by id
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if(note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+    console.log(note)
+    response.json(note)
+}) 
+
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
 })
 
 const PORT = 3001
